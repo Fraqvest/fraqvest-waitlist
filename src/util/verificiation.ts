95cfd296a24c298cda681b4da2d97f40d0b3bece
unknown <ifennamonanu70@gmail.com>
2024-06-10 08:53:05 +0100

    feat: implemented ref_code_validation

--- src/util/verificiation.ts
+++ src/util/verificiation.ts
@@ -6,6 +6,17 @@ export const emailExists = async (email: string, link: string) => {
   }
 };
 
+
+export const refrenceExists = async (refNo: string, link:string) => {
+  const reference = await fetch(`${link}/search?refrence_number=${refNo}`);
+  const isRef = await reference.json();
+  if(isRef.length > 0) {
+    return true
+  }else {
+    return false
+  }
+}
+
 export function validateEmail(mail: string) {
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
     return true;
